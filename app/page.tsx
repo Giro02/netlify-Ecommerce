a6c5a38547599c0a005f59766034a524c5bb1aab
DESKTOP-9NJ6LPQ\pedro <pedroarendt02@hotmail.com>
2024-02-09 17:42:12 -0300

    Prods for you carousel working

--- app/page.tsx
+++ app/page.tsx
@@ -10,29 +10,9 @@ export default async function Home() {
 
   return (
     <main>
-      <div className="w-[500px]">
-        <QiLogo className="text-9xl" colors={["#DFBC64", "#BC64DF"]} />
-        <QiLogoName
-          className="text-9xl"
-          colors={["#BC64DF", "#DFBC64", "#DF7F64"]}
-        />
+      <div>
+        <Carousel carousel={prodsForYou}></Carousel>
       </div>
-      <div className="items-center justify-center flex">
-        <div className="font-noto">減肥</div>
-        <div className="font-montse"> &nbsp; / Emagrecimento</div>
-      </div>
-      {categories &&
-        categories.map((category) => (
-          <div>
-            <div>{category._id}</div>
-            <div>{category.title}</div>
-            <img
-              src={category.categoryImage?.image}
-              alt={category.categoryImage?.alt}
-            />
-          </div>
-        ))}
-      <Carousel carousel={prodsForYou}></Carousel>
     </main>
   );
 }
