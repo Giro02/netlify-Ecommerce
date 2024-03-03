2144b9280d794f0a1b9d57c6952f155dbe8245f7
DESKTOP-9NJ6LPQ\pedro <pedroarendt02@hotmail.com>
2024-03-03 08:12:54 -0300

    Added scrollSnapList to Carousel

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -24,36 +24,37 @@ function Constructor({ carousel }: CarouselProps) {
   const [selected, setSelected] = useState(0);
   const [Saiba, setSaiba] = useState<number | null>(null);
 
-  const SlideElements = carousel.products.map((product, index) => (
+  const slideElements = carousel.products.map((product, index) => (
     <div key={index}>
-      <div
-        className={`rounded-xl relative p-4 cursor-pointer transition-all hover:shadow-xl w-[200px] h-[400px] md:w-[240px] md:h-[425px] flex items-center justify-center flex-col text-center`}
-        onMouseEnter={() => setSaiba(index)}
-        onMouseLeave={() => setSaiba(null)}
-      >
-        <div className="w-full h-[140px] md:h-[140px]">
-          <img
-            src={product.productImage.image}
-            className="w-full h-full object-contain"
-            alt={product.productImage.alt}
-          />
-        </div>
-        <div>
-          <p className="text-[16px] md:h-8 h-12 text-color-5 mt-4">
-            {product.title}
-          </p>
-        </div>
-        <div>
-          <p className="text-[13px] mt-2 h-16 text-color-5/70">
-            {product.description}
-          </p>
-        </div>
-        <div>
-          <div className="text-[16px] h-10 items-center text-center flex text-color-5 font-bold">
-            <p>R$ {product.price}</p>
+      <Link className="w-full" href={`/produtos/${product.slug.current}`}>
+        <div
+          className={`rounded-xl relative p-4 cursor-pointer transition-all hover:shadow-xl w-[200px] h-[400px] md:w-[240px] md:h-[425px] flex items-center justify-center flex-col text-center`}
+          onMouseEnter={() => setSaiba(index)}
+          onMouseLeave={() => setSaiba(null)}
+        >
+          <div className="w-full h-[140px] md:h-[140px]">
+            <img
+              src={product.productImage.image}
+              className="w-full h-full object-contain"
+              alt={product.productImage.alt}
+            />
           </div>
-        </div>
-        <Link className="w-full" href={`/produtos/${product.slug.current}`}>
+          <div>
+            <p className="text-[16px] md:h-8 h-12 text-color-5 mt-4">
+              {product.title}
+            </p>
+          </div>
+          <div>
+            <p className="text-[13px] mt-2 h-16 text-color-5/70">
+              {product.description}
+            </p>
+          </div>
+          <div>
+            <div className="text-[16px] h-10 items-center text-center flex text-color-5 font-bold">
+              <p>R$ {product.price}</p>
+            </div>
+          </div>
+
           <div
             className={`h-10 bg-color-1 ${
               Saiba === index ? "flex" : " flex sm:invisible "
@@ -61,8 +62,8 @@ function Constructor({ carousel }: CarouselProps) {
           >
             Saiba Mais
           </div>
-        </Link>
-      </div>
+        </div>
+      </Link>
     </div>
   ));
 
@@ -91,18 +92,16 @@ function Constructor({ carousel }: CarouselProps) {
     if (emblaApi) return emblaApi.canScrollNext();
   }, [emblaApi]);
 
-  // const [size, setSize] = useState();
-
-  // const Carouselsize = useCallback((emblaApi: any) => {
-  //   return emblaApi.scrollSnapList();
-  // }, []);
+  const carouselSize = useCallback(() => {
+    if (emblaApi) return emblaApi.scrollSnapList().length;
+  }, [emblaApi]);
 
   return (
     <div className="container py-12">
       <div className="flex relative px-[15px]">
         <div className="embla" ref={emblaRef}>
           <div className="embla__container  flex justify-start gap-1 py-8">
-            {SlideElements}
+            {slideElements}
           </div>
         </div>
 
@@ -127,12 +126,12 @@ function Constructor({ carousel }: CarouselProps) {
           <IoIosArrowForward size={35}></IoIosArrowForward>
         </div>
       </div>
-      <Dots Quantity={carousel.products.length} Selected={selected}></Dots>
+      <Dots Quantity={carouselSize()} Selected={selected}></Dots>
     </div>
   );
 }
 type Items = {
-  Quantity: number;
+  Quantity: any;
   Selected: number;
 };
 
